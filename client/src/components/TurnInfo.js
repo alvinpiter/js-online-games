6103d453723e7d17ddf40d1dd57f7c5ac953aeff
Alvin Piter <alpiter10@gmail.com>
2020-09-14 11:11:25 +0800

    Display reversi player as BLACK or WHITE, instead of B or W

--- client/src/components/TurnInfo.js
+++ client/src/components/TurnInfo.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 export default function TurnInfo(props) {
   const { player, currentPlayer } = props
+
+  //hacky. this should be done on server.
+  let renderedPlayer
+  if (player === 'B')
+    renderedPlayer = 'BLACK'
+  else if (player === 'W')
+    renderedPlayer = 'WHITE'
+  else
+    renderedPlayer = player
+
   return (
     <div className="text-center">
-      <p> You are playing as <span className="font-bold">{player}</span></p>
+      <p> You are playing as <span className="font-bold">{renderedPlayer}</span></p>
       {
         player === currentPlayer ?
         <p> It's your turn </p> :
